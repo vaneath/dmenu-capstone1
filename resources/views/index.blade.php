14e2935dffd82cbe4f0742886ab1088844ca0520
Vaneath <vaneathzz@gmail.com>
2023-05-09 13:52:07 +0700

    init project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <h1>
+        Welcome to the home page
+    </h1>
+</x-app-layout>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,42 @@
+<nav class="bg-blue flex justify-evenly items-center">
+    <div class="flex gap-5">
+        <a href="{{ route('login') }}"
+           class="text-sm px-7 py-2 transition ease-linear duration-300 bg-yellow text-white rounded-xl border-2 border-yellow hover:bg-transparent hover:scale-110 hover:text-yellow"
+        >
+            Login
+        </a>
+        <a href="{{ route('register') }}"
+           class="text-sm px-7 py-2 transition ease-linear duration-300 bg-yellow text-white rounded-xl border-2 border-yellow hover:bg-transparent hover:scale-110 hover:text-yellow"
+        >
+            Register
+        </a>
+    </div>
+    <div class="w-20 h-20">
+        <a href="/">
+            <img src="{{ asset('images/dmenu.png') }}" alt="">
+        </a>
+    </div>
+    <div class="flex gap-5">
+        <a href="{{ auth() ? route('profile.edit') : route('login')  }}"
+           class="text-sm px-7 py-2 transition ease-linear duration-300 bg-yellow text-white rounded-xl border-2 border-yellow hover:bg-transparent hover:scale-110 hover:text-yellow"
+        >
+            {{ auth()->user() ? ucwords(auth()->user()->name) : 'Login' }}
+        </a>
+        @auth
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit"
+                        class="text-sm px-7 py-2 transition ease-linear duration-300 bg-yellow text-white rounded-xl border-2 border-yellow hover:bg-transparent hover:scale-110 hover:text-yellow"
+                >
+                    Logout
+                </button>
+            </form>
+        @else
+            <a href="{{ route('register') }}"
+               class="text-sm px-7 py-2 transition ease-linear duration-300 bg-yellow text-white rounded-xl border-2 border-yellow hover:bg-transparent hover:scale-110 hover:text-yellow"
+            >
+                Register
+            </a>
+        @endauth
+    </div>
+</nav>
