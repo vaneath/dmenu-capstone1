6d76fbd1d78392abf1b2356a8fa320a137fc7fdc
Vaneath <vaneathzz@gmail.com>
2023-05-10 16:03:07 +0700

    added restaurant card

---
+++ resources/views/components/restaurant-card.blade.php
@@ -0,0 +1,23 @@
+<div class="flex-col w-48 h-56 px-2 py-3 bg-blue text-white rounded-xl shadow-lg shadow-blue-500">
+    <div class="flex justify-end gap-2">
+        <a href="#">
+                    <span class="material-symbols-outlined">
+                      qr_code
+                    </span>
+        </a>
+        <a href="#">
+                    <span class="material-symbols-outlined">
+                       settings
+                    </span>
+        </a>
+    </div>
+    <h3 class="mb-5 font-bold text-center text-xl">V Restaurant</h3>
+    <div class="w-36 mx-auto text-center flex-col space-y-3">
+        <div class="px-4 py-2 rounded-lg bg-orange-300">
+            <a href="#" >Payment</a>
+        </div>
+        <div class="px-4 py-2 rounded-lg bg-orange-300">
+            <a href="#">Edit Menu</a>
+        </div>
+    </div>
+</div>
--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -1,3 +1,13 @@
 <x-setting>
-        This is restaurant
+    <x-restaurant-card />
+    <div class="grid items-center w-48 h-56 px-2 py-3 bg-blue text-white rounded-xl">
+        <h3 class="mb-2 font-bold text-center text-md">Create Restaurant</h3>
+        <div class="mx-auto rounded-full bg-orange-300 w-16 h-16 flex items-center justify-center">
+            <button class="flex items-center justify-center">
+                <span class="material-symbols-outlined">
+                   add
+                </span>
+            </button>
+        </div>
+    </div>
 </x-setting>
