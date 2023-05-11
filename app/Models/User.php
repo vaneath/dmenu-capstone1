5fe4fd3288bf1a52f6761f59623ab568d1e2f3fd
Vaneath <vaneathzz@gmail.com>
2023-05-11 12:26:05 +0700

    edit restaurant create button

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,7 +43,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
     ];
 
-    public function restaurants()
+    public function restaurants(): HasMany
     {
         return $this->hasMany(Restaurant::class);
     }
