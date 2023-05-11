25641d35357e95ed00f8419e76032539f3bf211e
KKC73 <50138914+KKC73@users.noreply.github.com>
2023-05-11 14:18:18 +0700

    Add form validation

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -8,7 +8,15 @@ class SectionController extends Controller
 {
     public function index()
     {
-        //
+        // get section but only belongs to the restaurant
+        $sections = new Section();
+        $tables = $sections->getTable();
+        $section_columns = Schema::getColumnListing($tables);
+        $section_columns = array_diff($section_columns, ['created_at', 'updated_at']);
+        return view('section.index', [
+            'sections' => Section::where('user_id', Auth::id())->get(),
+            'section_columns' => $section_columns
+        ]);
     }
 
     public function store(Request $request)
--- resources/views/components/restaurant-create-modal.blade.php
+++ resources/views/components/restaurant-create-modal.blade.php
@@ -1,4 +1,36 @@
-<div x-data="{ open: false }"
+<div x-data="{ open: false,
+    isValid: { name: true, location: true, currency: true, no_of_tables: true, no_of_available_tables: true },
+    inputValues: { name: '', location: '', currency: '', no_of_tables: '', no_of_available_tables: '' },
+    submit() {
+      this.validateName();
+      this.validateLocation();
+      this.validateCurrency();
+      this.validateNoOfTables();
+      this.validateNoOfAvailableTables();
+
+      console.log(this.inputValues);
+
+      if (Object.values(this.isValid).every(Boolean)) {
+        event.target.submit();
+        this.createRestaurantFormOpen = false;
+      }
+    },
+    validateName() {
+      this.isValid.name = this.inputValues.name.trim() !== '';
+    },
+    validateLocation() {
+      this.isValid.location = this.inputValues.location.trim() !== '';
+    },
+    validateCurrency() {
+      this.isValid.currency = this.inputValues.currency.trim() !== '';
+    },
+    validateNoOfTables() {
+      this.isValid.no_of_tables = this.inputValues.no_of_tables !== '';
+    },
+    validateNoOfAvailableTables() {
+      this.isValid.no_of_available_tables = this.inputValues.no_of_available_tables.trim() !== '';
+    }
+ }"
   @create-restaurant-form-open.window="open = $event.detail.createRestaurantFormOpen">
 
 
@@ -6,20 +38,23 @@
       <div class="relative w-auto max-w-2xl mx-auto">
         <div class="bg-white w-full p-6 text-wrap break-words flex flex-col" style="max-height: 80vh; overflow-y: auto">
           <h2 class="text-2xl font-bold mb-4 text-center">Add Restaurant</h2>
-            <form action="/restaurant" method="POST">
+          <form @submit.prevent="submit" action="/restaurant" method="POST" x-data="{  }">
                 @csrf
             <div class="flex flex-col mb-4">
               <label class="text-gray-700 mb-2">Restaurant's Name:</label>
-              <input class="border border-gray-300 rounded-md px-3 py-2" type="text" placeholder="Wonderful Restaurant" name="name" />
+              <input @blur="validateName" class="border border-gray-300 rounded-md px-3 py-2" type="text" placeholder="Wonderful Restaurant" name="name" x-model="inputValues.name" />
+              <p x-show="!isValid.name" class="text-red-600 text-sm mt-1" id="name-required" x-cloak>This field is required.</p>
             </div>
-            <!-- google map location -->
             <div class="flex flex-col mb-4">
               <label class="text-gray-700 mb-2">Google Map Location:</label>
-              <input class="border border-gray-300 rounded-md px-3 py-2" type="text" placeholder="https://goo.gl/maps/..." name="location" />
+              <input @blur="validateLocation" class="border border-gray-300 rounded-md px-3 py-2" type="text" placeholder="https://goo.gl/maps/..." name="location" x-model="inputValues.location" />
+              <div x-show="!isValid.location" class="text-red-600 text-sm mt-1" id="location-required" x-cloak>This field is required.</div>
             </div>
             <div class="flex flex-col mb-4">
               <label class="text-gray-700 mb-2">Currency:</label>
-              <select class="border border-gray-300 rounded-md px-3 py-2" name="currency" >
+              <!-- select tag with default value Phnom Penh --> 
+
+              <select @blur="validateCurrency" class="border border-gray-300 rounded-md px-3 py-2" name="currency" x-model="inputValues.currency" >
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="GBP">GBP</option>
@@ -56,26 +91,23 @@
                 <option value="HUF">HUF</option>
                 <option value="SAR">SAR</option>
               </select>
+              <div x-show="!isValid.currency" class="text-red-600 text-sm mt-1" id="currency-required" x-cloak>This field is required.</div>
             </div>
             <div class="flex flex-col mb-4">
               <label class="text-gray-700 mb-2">Number of Table:</label>
-              <input class="border border-gray-300 rounded-md px-3 py-2" type="number" placeholder="6" name="no_of_tables" />
+              <input @blur="validateNoOfTables" class="border border-gray-300 rounded-md px-3 py-2" type="number" placeholder="6" name="no_of_tables" x-model="inputValues.no_of_tables"/>
+              <div x-show="!isValid.no_of_tables" class="text-red-600 text-sm mt-1" id="no-of-tables-required" x-cloak>This field is required.</div>
             </div>
             <div class="flex flex-col mb-4">
               <label class="text-gray-700 mb-2">Number of Available Table:</label>
-              <input class="border border-gray-300 rounded-md px-3 py-2" type="number" placeholder="5" name="no_of_available_tables" />
+              <input @blur="validateNoOfAvailableTables" class="border border-gray-300 rounded-md px-3 py-2" type="number" placeholder="5" name="no_of_available_tables" x-model="inputValues.no_of_available_tables" />
+              <div x-show="!isValid.no_of_available_tables" class="text-red-600 text-sm mt-1" id="no-of-available-tables-required" x-cloak>This field is required.</div>
             </div>
-
-            <!-- <div class="flex items-center mb-6"> -->
-            <!-- <input type="hidden" name="is_visible" value="false">
-              <input value="true" type="checkbox" class="form-checkbox h-5 w-5 text-green-500 mr-2" name="is_visible" x-model.lazy="is_visible" />
-              <label class="text-gray-700">Is Visible</label> -->
-            <!-- </div> -->
             <div class="flex justify-center space-x-4">
               <button type="button" class="bg-red-500 text-white rounded-lg px-6 py-2" @click="createRestaurantFormOpen = false">
                 Close
               </button>
-              <button type="submit" class="bg-green-500 text-white rounded-lg px-6 py-2" @click="createRestaurantFormOpen = false">
+              <button type="submit" class="bg-green-500 text-white rounded-lg px-6 py-2" >
                 Create
               </button>
             </div>
@@ -84,7 +116,5 @@
       </div>
     </div>
     <div x-show="createRestaurantFormOpen" class="absolute z-40 inset-0 bg-black bg-opacity-50"></div>
-
-
   
 </div>
