fb10cd34cad0e70d56b5d1f12bc6e72758548b3a
Solita Pun <solitapun888@gmail.com>
2023-05-16 22:43:39 +0700

    home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,25 @@
 <x-app-layout>
-    <h1>
-        Welcome to the home page
-    </h1>
+<!doctype html>
+<html>
+<head>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div>
+    <img src="{{ asset('images/background1.png') }}" alt="">
+    </div>
+    <div>
+        <h1>With Dmenu, you can enjoy the modern way of ordering food in a restaurant. Just scan the QR Code and your food will be at your table in no time. </h1>
+       
+    </div>
+    <div><video style="width:100%;" muted autoplay loop>
+    <source src="{{ asset('video/video1.mp4') }}" type="video/mp4" />
+</video>
+</div>
+</body>
+</html>
 </x-app-layout>
