48d688015b8ff7ddc9204ce1cf965a3e5b6a695d
Solita Pun <solitapun888@gmail.com>
2023-05-17 01:01:58 +0700

    add elements in home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,12 +19,9 @@
     <source src="{{ asset('video/video1.mp4') }}" type="video/mp4" />
 </video>
 </div>
-<div class="table">
-  <div id="a1">A1</div>
-  <div id="b1">B1</div>
-  <div id="a2">A2</div>
-  <div id="a3">A3</div>
-  <div id="b3">B3</div>
+<div><video style="width:100%;" muted autoplay loop>
+    <source src="{{ asset('video/video2.mp4') }}" type="video/mp4" />
+</video>
 </div>
 </body>
 </html>
