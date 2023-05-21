b8b9f0c6e3ec4865030e43c4c174b06566e0af4f
Solita Pun <solitapun888@gmail.com>
2023-05-21 11:46:07 +0700

    modified caption

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,16 +12,32 @@
     <div>
     <img src="{{ asset('images/background1.png') }}" alt="">
     </div>
-    <div class="home-image">
-    <img src="{{ asset('images/Home.png') }}" alt="">
+    <div class="textMiddle">
+        <h1 style="font-size: 40px; margin: 5em; color: white;">With Dmenu, you can enjoy the modern way of ordering food in a restaurant. Just scan the QR Code and your food will be at your table in no time.</h1>
     </div>
-    <div><video style="width:100%;" muted autoplay loop>
-    <source src="{{ asset('video/video1.mp4') }}" type="video/mp4" />
-</video>
-</div>
 <div><video style="width:100%;" muted autoplay loop>
     <source src="{{ asset('video/video2.mp4') }}" type="video/mp4" />
 </video>
+</div>
+
+<div class="row">
+  <div class="column">
+    <img src="wedding.jpg">
+    <img src="rocks.jpg">
+    <img src="falls2.jpg">
+    <img src="paris.jpg">
+    <img src="nature.jpg">
+    <img src="mist.jpg">
+    <img src="paris.jpg">
+  </div>
+  <div class="column">
+    <img src="underwater.jpg">
+    <img src="ocean.jpg">
+    <img src="wedding.jpg">
+    <img src="mountainskies.jpg">
+    <img src="rocks.jpg">
+    <img src="underwater.jpg">
+  </div>
 </div>
 </body>
 </html>
