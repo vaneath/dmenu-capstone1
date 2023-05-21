e03541cf2756f5ebe80b238a188ec8a14e584ecb
Solita Pun <solitapun888@gmail.com>
2023-05-21 15:10:11 +0700

    finalize home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,33 +12,18 @@
     <div>
     <img src="{{ asset('images/background1.png') }}" alt="">
     </div>
-    <div class="textMiddle">
-        <h1 style="font-size: 40px; margin: 5em; color: white;">With Dmenu, you can enjoy the modern way of ordering food in a restaurant. Just scan the QR Code and your food will be at your table in no time.</h1>
-    </div>
-<div><video style="width:100%;" muted autoplay loop>
+    <div class="example">
+        With Dmenu, you can enjoy the modern way of ordering food in a restaurant. Just scan the QR Code and your food will be at your table in no time.</div>
+<div>
+  <video style="width:100%;" muted autoplay loop>
     <source src="{{ asset('video/video2.mp4') }}" type="video/mp4" />
 </video>
 </div>
-
-<div class="row">
-  <div class="column">
-    <img src="wedding.jpg">
-    <img src="rocks.jpg">
-    <img src="falls2.jpg">
-    <img src="paris.jpg">
-    <img src="nature.jpg">
-    <img src="mist.jpg">
-    <img src="paris.jpg">
-  </div>
-  <div class="column">
-    <img src="underwater.jpg">
-    <img src="ocean.jpg">
-    <img src="wedding.jpg">
-    <img src="mountainskies.jpg">
-    <img src="rocks.jpg">
-    <img src="underwater.jpg">
-  </div>
-</div>
+<div class="example1">
+        Meet Our Amazing Team</div>
+<div style="width:100%;">
+    <img style="padding: 0; object-fit: cover; height: 100%; width:100%; "src="{{ asset('images/team.png') }}" alt="">
+    </div>
 </body>
 </html>
 </x-app-layout>
