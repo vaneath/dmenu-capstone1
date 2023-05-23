30fd3cdb9d3d82c869eb4bf765630c56099664ab
KKC73 <50138914+KKC73@users.noreply.github.com>
2023-05-23 11:23:11 +0700

    Modified Middleware

--- resources/views/components/restaurant/show-my-order.blade.php
+++ resources/views/components/restaurant/show-my-order.blade.php
@@ -17,72 +17,78 @@
         ],
     ];
 @endphp
-<div>
-    <div class="flex flex-col mt-4">
+<x-head>
+    <div>
+        <div class="flex flex-col mt-4">
 
-        <div class="flex justify-between items-center">
-            <h2 class="text-xl font-semibold">Order Details</h2>
-            <a href="#" class="text-gray-600 hover:text-gray-900">View Order History</a>
-        </div>
-        <div class="information-wrapper flex flex-wrap">
-            <div class="column-wrapper w-1/2 flex flex-wrap">
-                <div class="flex justify-between">
-                    <div class="w-full">
-                        <h3 class="text-lg font-semibold">Order Number</h3>
-                        <p class="text-gray-500">1234567890</p>
+            <div class="flex justify-between items-center">
+                <h2 class="text-xl font-semibold">Order Details</h2>
+                <a href="#" class="text-gray-600 hover:text-gray-900">View Order History</a>
+            </div>
+            <div class="information-wrapper flex flex-wrap">
+                <div class="column-wrapper w-1/2 flex flex-wrap">
+                    <div class="flex justify-between">
+                        <div class="w-full">
+                            <h3 class="text-lg font-semibold">Order Number</h3>
+                            <p class="text-gray-500">1234567890</p>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-between">
+                        <div class="w-full">
+                            <h3 class="text-lg font-semibold">Shipping Address</h3>
+                            <p class="text-gray-500">123 Main Street</p>
+                            <p class="text-gray-500">Anytown, CA 12345</p>
+                        </div>
                     </div>
                 </div>
 
-                <div class="flex justify-between">
-                    <div class="w-full">
-                        <h3 class="text-lg font-semibold">Shipping Address</h3>
-                        <p class="text-gray-500">123 Main Street</p>
-                        <p class="text-gray-500">Anytown, CA 12345</p>
+                <div class="column-wrapper w-1/2">
+                    <div class="flex justify-between">
+                        <div class="w-full">
+                            <h3 class="text-lg font-semibold">Order Date</h3>
+                            <p class="text-gray-500">2023-05-21</p>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-between">
+                        <div class="w-full">
+                            <h3 class="text-lg font-semibold">Billing Address</h3>
+                            <p class="text-gray-500">123 Main Street</p>
+                            <p class="text-gray-500">Anytown, CA 12345</p>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="column-wrapper w-1/2">
-                <div class="flex justify-between">
-                    <div class="w-full">
-                        <h3 class="text-lg font-semibold">Order Date</h3>
-                        <p class="text-gray-500">2023-05-21</p>
-                    </div>
-                </div>
-
-                <div class="flex justify-between">
-                    <div class="w-full">
-                        <h3 class="text-lg font-semibold">Billing Address</h3>
-                        <p class="text-gray-500">123 Main Street</p>
-                        <p class="text-gray-500">Anytown, CA 12345</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Item Name</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($items as $item)
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $item['name'] }}</td>
-                        <td>{{ $item['quantity'] }}</td>
-                        <td>{{ $item['price'] }}</td>
+                        <th>Item Name</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody class="text-center mt-10">
+                    @foreach ($items as $item)
+                        <tr>
+                            <td>{{ $item['name'] }}</td>
+                            <td>{{ $item['quantity'] }}</td>
+                            <td>{{ $item['price'] }}</td>
+                        </tr>
+                    @endforeach
+                    <tr>
+                        <td colspan="2" class="text-left">Subtotal</td>
+                        <td>$60.00</td>
+                    </tr>
+                 </tbody>
+            </table>
 
 
-        <div class="mt-4">
-            <h3 class="text-lg font-semibold">Order Total</h3>
-            <p class="text-gray-500">$30.00</p>
+            <div class="mt-4">
+                <h3 class="text-lg font-semibold">Order Total</h3>
+                <p class="text-gray-500">$30.00</p>
+            </div>
         </div>
     </div>
-</div>
+</x-head>
