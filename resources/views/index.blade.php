82a7ce38957ff79ba1a2b841343809d2db1c40e8
Vaneath <vaneathzz@gmail.com>
2023-05-24 10:22:46 +0700

    add footer only for homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,67 +1,71 @@
-<x-app-layout>
-<!doctype html>
-<html>
-<head>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-   
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
+<x-app-layout :footer="true">
+    <!doctype html>
+    <html>
+    <head>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
     <div>
-    <img src="{{ asset('images/background1.png') }}" alt="">
+        <img src="{{ asset('images/background1.png') }}" alt="">
     </div>
     <div class="example">
-        With Dmenu, you can enjoy the modern way of ordering food in a restaurant. Just scan the QR Code and your food will be at your table in no time.</div>
-<div>
-  <video style="width:100%;" muted autoplay loop>
-    <source src="{{ asset('video/video2.mp4') }}" type="video/mp4" />
-</video>
-</div>
+        With Dmenu, you can enjoy the modern way of ordering food in a restaurant. Just scan the QR Code and your food
+        will be at your table in no time.
+    </div>
+    <div>
+        <video style="width:100%;" muted autoplay loop>
+            <source src="{{ asset('video/video2.mp4') }}" type="video/mp4"/>
+        </video>
+    </div>
 
-<div class="wrapper">
-    <div class="priceplan">
-        Dmenu service plans
-    </div>
-    <div class="fee">
-        No hidden fees. No contract.
-    </div>
+    <div class="wrapper">
+        <div class="priceplan">
+            Dmenu service plans
+        </div>
+        <div class="fee">
+            No hidden fees. No contract.
+        </div>
         <div class="services">
-        <a href="#">
+            <a href="#">
             <span class="single-img img-one">
                 <span class="img-text">
                     <p>This is plan is best for large businesses.</p>
                     <button>Buy Now</button>
                 </span>
             </span>
-        </a>
-        <a href="#">
-            <span class="single-img img-two"> 
+            </a>
+            <a href="#">
+            <span class="single-img img-two">
                 <span class="img-text">
-                    
+
                     <p>This is plan is best for entrepreneurs.</p>
                     <button>Buy Now</button>
                 </span>
             </span>
-        </a>
-        <a href="#">
+            </a>
+            <a href="#">
             <span class="single-img img-three">
                 <span class="img-text">
-                    
+
                     <p>This is plan is best for small businesses.</p>
                     <button>Buy Now</button>
                 </span>
             </span>
-        </a>
-    </div>
+            </a>
+        </div>
     </div>
 
-<div class="example1">
-        Meet Our Amazing Team</div>
-<div style="width:100%;">
-    <img style="padding: 0; object-fit: cover; height: 100%; width:100%; "src="{{ asset('images/team.png') }}" alt="">
+    <div class="example1">
+        Meet Our Amazing Team
     </div>
-</body>
-</html>
+    <div style="width:100%;">
+        <img style="padding: 0; object-fit: cover; height: 100%; width:100%; " src="{{ asset('images/team.png') }}"
+             alt="">
+    </div>
+    </body>
+    </html>
 </x-app-layout>
