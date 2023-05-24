a14328e7b5f29170928088ff8acd422433171ba1
KKC73 <50138914+KKC73@users.noreply.github.com>
2023-05-24 10:25:16 +0700

    Mandatory Commit

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -12,13 +12,40 @@ class QrCodeController extends Controller
 {
     public function index($unique_id)
     {
+        // dd('hi');
         $restaurant = Restaurant::where('unique_id', $unique_id)->first();
+        $sections = $restaurant->sections;
         if ($restaurant == null) {
             abort(404);
         }
-        return redirect()->route('restaurant.menu', [
-            'restaurant' => $restaurant,
-            'sections' => $restaurant->sections,
-        ]);
+        $sections = $sections->sortBy('sort_number');
+        $activeSectionPage = request()->query('active-section-id');
+        if ($activeSectionPage != null && $sections->contains('id', $activeSectionPage)) {
+            return view('category.index', [
+                'restaurant' => $restaurant,
+                'sections' => $sections,
+                'activeSectionPage' => $activeSectionPage,
+            ]);
+        } elseif ($activeSectionPage != null && !$sections->contains('id', $activeSectionPage)) {
+            return view('category.index', [
+                'restaurant' => $restaurant,
+                'sections' => $sections,
+                'activeSectionPage' => $sections->first()->id,
+            ]);
+        } else{
+            if ($sections->isEmpty()) {
+                return view('category.index', [
+                    'restaurant' => $restaurant,
+                    'sections' => $sections,
+                    'activeSectionPage' => null,
+                ]);
+            } else {
+                return view('category.index', [
+                    'restaurant' => $restaurant,
+                    'sections' => $sections,
+                    'activeSectionPage' => $sections->first()->id,
+                ]);
+            }
+        }
     }
 }
