cdde8586218ba1c2c89739d2ada568514e35b3f0
KKC73 <50138914+KKC73@users.noreply.github.com>
2023-06-01 01:41:14 +0700

    Working With ImageController

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -57,6 +57,29 @@ class ImageController extends Controller
         // return 'hi';
         // Storage::disk('do-spaces')->putFile('uploads', $request->file('logo'), 'public');
         $filePath = $request->logo;
+
+        $client_id = "c24e5980b0c2515";
+        $image = file_get_contents($file_path);
+        // dd($image_path, file_exists($image_path));
+        $title = "My Image";
+        $description = "This is my image.";
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://api.imgur.com/3/image');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, [
+            'Authorization' => 'Client-ID ' . $client_id,
+            'Content-Type' => 'multipart/form-data',
+            'image' => base64_encode($image),
+            'title' => $title,
+            'description' => $description,
+        ]);
+
+        $response = curl_exec($ch);
+
+        dd($response);
         
         $file = file_get_contents($filePath);
 
