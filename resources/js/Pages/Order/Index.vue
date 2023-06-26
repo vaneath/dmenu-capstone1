a7c86186d6593e599eae6c1fc216e5af803906f6
Vaneath <vaneathzz@gmail.com>
2023-06-26 08:58:19 +0700

    remove 30 days filter

--- resources/js/Pages/Order/Index.vue
+++ resources/js/Pages/Order/Index.vue
@@ -46,17 +46,8 @@ onUnmounted(() => {
     <Head title="Orders" />
 
     <AuthenticatedLayout>
-
-        <p v-text="selected_restaurant"></p>
-
         <div class="relative overflow-x-auto sm:rounded-lg">
             <div>
-                <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:focus:ring-gray-700" type="button">
-                    <svg class="w-4 h-4 mr-2 text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
-                    Last 30 days
-                    <svg class="w-3 h-3 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
-                </button>
-
                 <!-- filter by restaurant -->
                 <div>
                     <select id="filter-by-restaurant" v-model="selected_restaurant" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:focus:ring-gray-700" type="button">
