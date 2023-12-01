e745e42f4332939c2c3909660d806fd4c6eef784
vaneath <vaneathzz@gmail.com>
2023-12-01 15:15:48 +0700

    close #6 configure seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,17 +8,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        $this->call([
-            UserSeeder::class,
-            RestaurantSeeder::class,
-            TagSeeder::class,
-            CategorySeeder::class,
-            ItemSeeder::class,
-        ]);
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    $this->call([
+      UserSeeder::class,
+    ]);
+  }
 }
